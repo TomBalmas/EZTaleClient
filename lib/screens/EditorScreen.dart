2b9d6cf74c0150d15d5435991655384c9489155e
TalAmar <f914gt29ra12@gmail.com>
2022-11-12 10:22:33 +0200

    lets go

--- lib/screens/EditorScreen.dart
+++ lib/screens/EditorScreen.dart
@@ -2,6 +2,7 @@ import 'package:ez_tale/EZNetworking.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
+import '../constants.dart';
 import '../widgets/Widgets.dart';
 
 class EditorScreen extends StatelessWidget {
@@ -9,23 +10,70 @@ class EditorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double buttonWidth = 300, buttonHeight = 60;
     return Scaffold(
-        drawer: EZDrawer(),
-        appBar: AppBar(title: Text('Book1')),
-        body: Center(
-          child: Column(
-            children: [
-              quill.QuillToolbar.basic(controller: _controller),
-              Expanded(
-                child: Container(
-                  child: quill.QuillEditor.basic(
-                    controller: _controller,
-                    readOnly: false, // true for view only mode
-                  ),
+      drawer: EZDrawer(),
+      appBar: AppBar(title: Text('book1')), //TODO correct the name
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(left: 100, right: 100, bottom: 100),
+            child: Column(
+              children: [
+                SizedBox(height: 16),
+                Row(  //FIXME overflow
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    buildButton('Send', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
+                    SizedBox(width: 16),
+                    buildButton('Update', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
+                    SizedBox(width: 16),
+                    buildButton('Merge Requests', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
+                  ]
                 ),
-              )
-            ],
+                SizedBox(height: 16),
+                quill.QuillToolbar.basic(controller: _controller),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 5, color: Colors.blue),
+                      color: Colors.white
+                    ),
+                    child: quill.QuillEditor.basic(
+                      controller: _controller,
+                      readOnly: false, // true for view only mode
+                    ),
+                  ),
+                )
+              ],
           ),
-        ));
+        )
+      )
+    );
+
   }
+}
+
+
+buildButton(String buttonName, Color bgColor, Color textColor, double width, double height, Function onTap){
+    return SizedBox(
+        width: width,
+        child: Container(
+            height: height,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: bgColor,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.resolveWith(
+                  (states) => Colors.black12,
+                ),
+              ),
+              onPressed: onTap,
+              child: Text(
+                buttonName,
+                style: kButtonText.copyWith(color: textColor),
+              ),
+            )));
 }
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../widgets/Widgets.dart';
+import 'EditorScreen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({key});
@@ -15,7 +17,18 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: kBackgroundColor,
       ),
       body: Center(
-        
+        child: EZTextButton(
+          buttonName: 'Editor Screen', // TODO correct the button
+          onTap: (){
+            Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => EditorScreen()));
+                      return;
+          },
+          bgColor: Color.fromRGBO(0, 173, 181, 100),
+          textColor: Colors.black87
+        )
       )
     );
   }
