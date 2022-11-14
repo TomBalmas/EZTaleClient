1e0d242e352fb1346ffcad3f764790a91385e651
TalAmar <f914gt29ra12@gmail.com>
2022-11-14 07:53:50 +0200

    8:00 14.11

--- lib/screens/EditorScreen.dart
+++ lib/screens/EditorScreen.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:ez_tale/EZNetworking.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,15 +7,21 @@ import 'package:flutter_quill/flutter_quill.dart' as quill;
 import '../constants.dart';
 import '../widgets/Widgets.dart';
 
-class EditorScreen extends StatelessWidget {
+
+class EditorScreen extends StatefulWidget {
+  @override
+  State<EditorScreen> createState() => _EditorScreenState();
+}
+
+class _EditorScreenState extends State<EditorScreen> {
   quill.QuillController _controller = quill.QuillController.basic();
+  Color repeatedWordsColor = Colors.red, tenseTrackingColor = Colors.red, turningPointsColor = Colors.red;
 
   @override
   Widget build(BuildContext context) {
-    double buttonWidth = 300, buttonHeight = 60;
     return Scaffold(
       drawer: EZDrawer(),
-      appBar: AppBar(title: Text('book1')), //TODO correct the name
+      appBar: AppBar(title: Text('book1')), //TODO: correct the name
       body: Container(
         child: Padding(
           padding: EdgeInsets.all(16),
@@ -21,44 +29,96 @@ class EditorScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
+                flex: 2,
                 child:Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          BuildButton('Characters', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Characters', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Locations', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Conversations', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Custom', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Attribute Templates', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                          SizedBox(height: 16),
-                          BuildButton('Co Writers', Color.fromRGBO(0, 173, 181, 100), Colors.black87, buttonWidth, buttonHeight, (){}),
-                        ]),
-                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BuildButton(
+                          name: 'Characters',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                        SizedBox(height: 16),
+                        BuildButton(
+                          name: 'Locations',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                        SizedBox(height: 16),
+                        BuildButton(
+                          name: 'Conversations',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                        SizedBox(height: 16),
+                        BuildButton(
+                          name: 'Custom',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                        SizedBox(height: 16),
+                        BuildButton(
+                          name: 'Attribute Templates',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                        SizedBox(height: 16),
+                        BuildButton(
+                          name: 'Co-Writers',
+                          bgColor: Color.fromRGBO(0, 173, 181, 100),
+                          textColor: Colors.black87,
+                          onTap: (){}
+                        ),
+                      ]),
+                    SizedBox(width: 16),
                     Expanded(
                       child: Column(
                         children: [
                           Expanded(
+                            flex: 0,
                             child: Row(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                BuildButton('Send', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 100, buttonHeight, (){}),
+                                BuildButton(
+                                  name: 'Send',
+                                  bgColor: Color.fromRGBO(0, 173, 181, 100),
+                                  textColor: Colors.black87,
+                                  onTap: (){},
+                                  width: 100,
+                                ),
                                 SizedBox(width: 16),
-                                BuildButton('Update', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 100, buttonHeight, (){}),
+                                BuildButton(
+                                  name: 'Update',
+                                  bgColor: Color.fromRGBO(0, 173, 181, 100),
+                                  textColor: Colors.black87,
+                                  onTap: (){},
+                                  width: 100,
+                                ),
                                 SizedBox(width: 16),
-                                BuildButton('Merge Requests', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 100, buttonHeight, (){}),
+                                BuildButton(
+                                  name: 'Merge Requests',
+                                  bgColor: Color.fromRGBO(0, 173, 181, 100),
+                                  textColor: Colors.black87,
+                                  onTap: (){},
+                                  width: 100,
+                                ),
                               ]),
                             ),
                             SizedBox(height: 16),
-                            quill.QuillToolbar.basic(controller: _controller),
+                            quill.QuillToolbar.basic(
+                              controller: _controller,
+                              showFontFamily: false,
+                              showFontSize: false,
+                            ),
+                            SizedBox(height: 16),
                             Expanded(
                               child: Container(
                                 decoration: BoxDecoration(
@@ -77,6 +137,7 @@ class EditorScreen extends StatelessWidget {
               ),
               Expanded(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
                       width: 450,
@@ -85,27 +146,72 @@ class EditorScreen extends StatelessWidget {
                         border: Border.all(width: 5, color: Colors.blue),
                         color: Colors.white
                       ),
-                      child: Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Editor Options:'),
-                            Expanded(
-                              child: Row(
-                                children: [
-                                  BuildButton('Repeated Words', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 125, buttonHeight, (){}),
-                                  SizedBox(width: 16),
-                                  BuildButton('Tense Tracking', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 125, buttonHeight, (){}),
-                                  SizedBox(width: 16),
-                                  BuildButton('Turning Point', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 125, buttonHeight, (){}),
-                                ])
-                            )
-                          ],
-                        )
-                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 7),
+                          Text('Editor Options:'),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              BuildButton(
+                                name: 'Repeated Words',
+                                bgColor: repeatedWordsColor,
+                                textColor: Colors.black87,
+                                onTap: (){
+                                  setState(() {
+                                    if(repeatedWordsColor == Colors.red)
+                                      repeatedWordsColor = Colors.green;
+                                    else
+                                      repeatedWordsColor = Colors.red;
+                                  });
+                                },
+                                width: 100,
+                              ),
+                              SizedBox(width: 16),
+                              BuildButton(
+                                name: 'Tense Tracking',
+                                bgColor: tenseTrackingColor,
+                                textColor: Colors.black87,
+                                onTap: (){
+                                  setState(() {
+                                    if(tenseTrackingColor == Colors.red)
+                                      tenseTrackingColor = Colors.green;
+                                    else
+                                      tenseTrackingColor = Colors.red;
+                                  });
+                                },
+                                width: 100,
+                              ),
+                              SizedBox(width: 16),
+                              BuildButton(
+                                name: 'Turning Points',
+                                bgColor: turningPointsColor,
+                                textColor: Colors.black87,
+                                onTap: (){
+                                  setState(() {
+                                    if(turningPointsColor == Colors.red)
+                                      turningPointsColor = Colors.green;
+                                    else
+                                      turningPointsColor = Colors.red;
+                                  });
+                                },
+                                width: 100,
+                              )
+                          ])
+                        ],
+                      )
                     ),
                     SizedBox(width: 16),
-                    BuildButton('Manual Save', Color.fromRGBO(0, 173, 181, 100), Colors.black87, 100, buttonHeight, (){}),
+                    BuildButton(
+                      name: 'Manual Save',
+                      bgColor: Color.fromRGBO(0, 173, 181, 100),
+                      textColor: Colors.black87,
+                      onTap: (){},
+                      height: 100,
+                    ),
+                    SizedBox(width: 16),
                   ])
               )
           ]),
@@ -116,30 +222,51 @@ class EditorScreen extends StatelessWidget {
   }
 }
 
-Widget BuildButton(String name, Color bgColor, Color textColor, double width, double height, Function onTap){
-  return Flexible(
-    child: SizedBox(
-      width: width,
-      child: Container(
-        height: height,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: bgColor,
-          borderRadius: BorderRadius.circular(18),
-        ),
-        child: TextButton(
-          style: ButtonStyle(
-            overlayColor: MaterialStateProperty.resolveWith(
-              (states) => Colors.black12,
+
+
+class BuildButton extends StatelessWidget{
+  BuildButton({
+    Key key,
+    @required this.name,
+    this.onTap,
+    @required this.bgColor,
+    @required this.textColor,
+    this.height = 60,
+    this.width = 300,
+  }) : super(key: key);
+  final String name;
+  final Function onTap;
+  final Color bgColor;
+  final Color textColor;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+      return Flexible(
+      child: SizedBox(
+        width: width,
+        child: Container(
+          height: height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: TextButton(
+            style: ButtonStyle(
+              overlayColor: MaterialStateProperty.resolveWith(
+                (states) => Colors.black12,
+              ),
             ),
-          ),
-          onPressed: onTap,
-          child: Text(
-            name,
-            style: kButtonText.copyWith(color: textColor),
-          ),
+            onPressed: onTap,
+            child: Text(
+              name,
+              style: kButtonText.copyWith(color: textColor),
+            ),
+          )
         )
       )
-    )
-  );
+    );
+  }
 }
